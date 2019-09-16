e57eeb8faacd4f3917e7091c44df9fdcf1762c17
山田 俊輔 <yshunske@gmail.com>
2019-09-16 22:46:56 +0900

    add:memory_profilerを使った計測

--- app/services/measure_service.rb
+++ app/services/measure_service.rb
@@ -1,7 +1,8 @@
 require 'benchmark/ips'
+require 'memory_profiler'
 
 class MeasureService
-  def measure_ten_columns
+  def measure_ten_columns_by_ips
     # StackProf.run(mode: :object, out: 'tmp/stackprof-object-myapp.dump', interval: 1) do
     #   @ten_columns = TenColumn.limit(30)
     #   @ten_columns.to_a
@@ -32,7 +33,7 @@ class MeasureService
     end
   end
 
-  def measure_thirty_columns
+  def measure_thirty_columns_by_ips
     Benchmark.ips do |x|
       x.report("all") do
         ten_columns = ThirtyColumn.limit(30)
@@ -57,5 +58,63 @@ class MeasureService
 
       x.compare!
     end
+  end
+
+  def measure_ten_columns_by_memory_profiler
+    report = MemoryProfiler.report do
+      ten_columns = TenColumn.limit(30)
+      ten_columns.each do |model|
+        "#{model.item1} #{model.item2}"
+      end
+    end
+
+    report.pretty_print
+
+    # report = MemoryProfiler.report do
+    #   ten_columns = TenColumn.limit(30).select(:item1, :item2)
+    #   ten_columns.each do |model|
+    #     "#{model.item1} #{model.item2}"
+    #   end
+    # end
+
+    # report.pretty_print
+
+    # report = MemoryProfiler.report do
+    #   ten_columns = TenColumn.limit(30).pluck(:item1, :item2)
+    #   ten_columns.each do |model|
+    #     "#{model[0]} #{model[1]}"
+    #   end
+    # end
+
+    # report.pretty_print
+  end
+
+  def measure_thirty_columns_by_memory_profiler
+    report = MemoryProfiler.report do
+      ten_columns = ThirtyColumn.limit(30)
+      ten_columns.each do |model|
+        "#{model.item1} #{model.item2}"
+      end
+    end
+
+    report.pretty_print
+
+    # report = MemoryProfiler.report do
+    #   ten_columns = ThirtyColumn.limit(30).select(:item1, :item2)
+    #   ten_columns.each do |model|
+    #     "#{model.item1} #{model.item2}"
+    #   end
+    # end
+
+    # report.pretty_print
+
+    # report = MemoryProfiler.report do
+    #   ten_columns = ThirtyColumn.limit(30).pluck(:item1, :item2)
+    #   ten_columns.each do |model|
+    #     "#{model[0]} #{model[1]}"
+    #   end
+    # end
+
+    # report.pretty_print
   end
 end
