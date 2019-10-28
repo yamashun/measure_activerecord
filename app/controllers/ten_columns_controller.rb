022bc4b6d13b07bbedb1c6b5c4d323d0e1823b06
山田 俊輔 <yshunske@gmail.com>
2019-10-28 17:53:05 +0900

    wip 計測テスト

--- app/controllers/ten_columns_controller.rb
+++ app/controllers/ten_columns_controller.rb
@@ -1,9 +1,9 @@
 class TenColumnsController < ApplicationController
   def index
-    StackProf.run(mode: :cpu, out: 'tmp/stackprof-cpu-myapp.dump') do
+    # StackProf.run(mode: :cpu, out: 'tmp/stackprof-cpu-myapp.dump') do
     # StackProf.run(mode: :object, out: 'tmp/stackprof-object-myapp.dump', interval: 1) do
       @ten_columns = TenColumn.limit(30)
       @ten_columns.to_a
-    end
+    # end
   end
 end
--- app/services/measure_service.rb
+++ app/services/measure_service.rb
@@ -2,28 +2,28 @@ require 'benchmark/ips'
 require 'memory_profiler'
 
 class MeasureService
-  def measure_ten_columns_by_ips
+  def measure_ten_columns_by_ips(limit)
     # StackProf.run(mode: :object, out: 'tmp/stackprof-object-myapp.dump', interval: 1) do
     #   @ten_columns = TenColumn.limit(30)
     #   @ten_columns.to_a
     # end
     Benchmark.ips do |x|
       x.report("all") do
-        ten_columns = TenColumn.limit(30)
+        ten_columns = TenColumn.limit(limit)
         ten_columns.each do |model|
           "#{model.item1} #{model.item2}"
         end
       end
 
       x.report("select") do
-        ten_columns = TenColumn.limit(30).select(:item1, :item2)
+        ten_columns = TenColumn.limit(limit).select(:item1, :item2)
         ten_columns.each do |model|
           "#{model.item1} #{model.item2}"
         end
       end
 
       x.report("pluck") do
-        ten_columns = TenColumn.limit(30).pluck(:item1, :item2)
+        ten_columns = TenColumn.limit(limit).pluck(:item1, :item2)
         ten_columns.each do |model|
           "#{model[0]} #{model[1]}"
         end
@@ -33,10 +33,10 @@ class MeasureService
     end
   end
 
-  def measure_thirty_columns_by_ips
+  def measure_thirty_columns_by_ips(limit)
     Benchmark.ips do |x|
       x.report("all") do
-        ten_columns = ThirtyColumn.limit(30)
+        ten_columns = ThirtyColumn.limit(limit)
         ten_columns.each do |model|
           "#{model.item1} #{model.item2}"
         end
